b52bb57455fbe4fc975bc5b55e296723138693ad
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2022-04-18 01:10:26 +0500

    remove magic numbers in EnumSwitch

--- src/main/java/enums/EnumSwitch.java
+++ src/main/java/enums/EnumSwitch.java
@@ -6,7 +6,30 @@ package enums;
  */
 public class EnumSwitch {
     //test
-    public static final double finalBidNoMagic = 0.11;
+    public static final double finalBidNoMagic_0_1 = 0.1; //2
+    public static final double finalBidNoMagic_0_12 = 0.12; //1
+    public static final double finalBidNoMagic_0_11 = 0.11; //2
+    public static final double finalBidNoMagic_0_15 = 0.15; //1
+    public static final double finalBidNoMagic_0_165 = 0.165; //1
+    public static final double finalBidNoMagic_0_18 = 0.18; //1
+    public static final double finalBidNoMagic_0_198 = 0.198; //1
+    public static final double finalBidNoMagic_0_2 = 0.2; //2
+    public static final double finalBidNoMagic_0_216 = 0.216; //1
+    public static final double finalBidNoMagic_0_22 = 0.22; //1
+    public static final double finalBidNoMagic_0_24 = 0.24; //1
+    public static final double finalBidNoMagic_0_3 = 0.3; //3
+    public static final double finalBidNoMagic_0_5 = 0.5; //1
+    public static final double finalBidNoMagic_0_7 = 0.7; //1
+    public static final double finalBidNoMagic_2 = 2; //8
+
+    public static final double condition_4000000 = 4000000; //4
+    public static final double condition_7000000 = 7000000; //4
+    public static final double condition_10000000 = 10000000; //6
+    public static final double condition_300000000 = 300000000; //10
+    public static final double condition_20000000 = 20000000; //2
+    public static final double condition_50000000 = 50000000; //2
+
+
     //test
 
     // Объявление числовой переменной, хранящей значение налоговой ставки
@@ -52,18 +75,18 @@ public class EnumSwitch {
             public double changeFinalBid() {
                 System.out.println("UfaRoom_UfaApartment");
                 //FIXME 100000000 ---> 10000000 УБРАЛ  ОДИН 0 для бывшего UfaRoom
-                if (cadastralValue <= 4000000) {
-                    return 0.11;
-                } else if ((cadastralValue <= 7000000) & (4000000 < cadastralValue)) {
-                    return 0.165;
-                } else if ((cadastralValue <= 10000000) & (7000000 < cadastralValue)) {
-                    return 0.198;
-                } else if (cadastralValue > 10000000) {
-                    return 0.22;
+                if (cadastralValue <= condition_4000000) {
+                    return finalBidNoMagic_0_11;
+                } else if ((cadastralValue <= condition_7000000) & (condition_4000000 < cadastralValue)) {
+                    return finalBidNoMagic_0_165;
+                } else if ((cadastralValue <= condition_10000000) & (condition_7000000 < cadastralValue)) {
+                    return finalBidNoMagic_0_198;
+                } else if (cadastralValue > condition_10000000) {
+                    return finalBidNoMagic_0_22;
                     //TODO что это
-                } else if (cadastralValue > 300000000) {
+                } else if (cadastralValue > condition_300000000) {
                 }
-                return 2;
+                return finalBidNoMagic_2;
             }
         },
         UfaHouse { //2
@@ -71,17 +94,17 @@ public class EnumSwitch {
             @Override
             public double changeFinalBid() {
                 System.out.println("UfaHouse");
-                if (cadastralValue <= 4000000) {
-                    return 0.12;
-                } else if ((cadastralValue <= 7000000) & (4000000 < cadastralValue)) {
-                    return 0.18;
-                } else if ((cadastralValue <= 10000000) & (7000000 < cadastralValue)) {
-                    return 0.216;
-                } else if (cadastralValue > 10000000) {
-                    return 0.24;
-                } else if (cadastralValue > 300000000) {
+                if (cadastralValue <= condition_4000000) {
+                    return finalBidNoMagic_0_12;
+                } else if ((cadastralValue <= condition_7000000) & (condition_4000000 < cadastralValue)) {
+                    return finalBidNoMagic_0_18;
+                } else if ((cadastralValue <= condition_10000000) & (condition_7000000 < cadastralValue)) {
+                    return finalBidNoMagic_0_216;
+                } else if (cadastralValue > condition_10000000) {
+                    return finalBidNoMagic_0_24;
+                } else if (cadastralValue > condition_300000000) {
                 }
-                return 2;
+                return finalBidNoMagic_2;
             }
         },
         UfaCar { //3
@@ -89,10 +112,10 @@ public class EnumSwitch {
             @Override
             public double changeFinalBid() {
                 System.out.println("UfaCar");
-                if (cadastralValue > 300000000) {
-                    return 2;
+                if (cadastralValue > condition_300000000) {
+                    return finalBidNoMagic_2;
                 } else
-                    return 0.11;
+                    return finalBidNoMagic_0_11;
             }
         },
 
@@ -104,10 +127,10 @@ public class EnumSwitch {
             @Override
             public double changeFinalBid() {
                 System.out.println("UfaOthers_KazanOthers_MoscowOthers");
-                if (cadastralValue > 300000000) {
-                    return 2;
+                if (cadastralValue > condition_300000000) {
+                    return finalBidNoMagic_2;
                 } else
-                    return 0.5;
+                    return finalBidNoMagic_0_5;
             }
         },
         /**
@@ -118,10 +141,10 @@ public class EnumSwitch {
             @Override
             public double changeFinalBid() {
                 System.out.println("KazanRoom_KazanApartment");
-                if (cadastralValue > 300000000) {
-                    return 2;
+                if (cadastralValue > condition_300000000) {
+                    return finalBidNoMagic_2;
                 } else
-                    return 0.2;
+                    return finalBidNoMagic_0_2;
             }
         },
 
@@ -134,10 +157,10 @@ public class EnumSwitch {
             @Override
             public double changeFinalBid() {
                 System.out.println("KazanHouse_KazanCar");
-                if (cadastralValue > 300000000) {
-                    return 2;
+                if (cadastralValue > condition_300000000) {
+                    return finalBidNoMagic_2;
                 } else
-                    return 0.3;
+                    return finalBidNoMagic_0_3;
             }
         },
         /**
@@ -148,17 +171,17 @@ public class EnumSwitch {
             @Override
             public double changeFinalBid() {
                 System.out.println("MoscowRoom_MoscowApartment_MoscowHouse");
-                if (cadastralValue <= 10000000) {
-                    return 0.1;
-                } else if ((cadastralValue <= 20000000) & (10000000 < cadastralValue)) {
-                    return 0.15;
-                } else if ((cadastralValue <= 50000000) & (20000000 < cadastralValue)) {
-                    return 0.2;
-                } else if ((cadastralValue <= 300000000) & (50000000 < cadastralValue)) {
-                    return 0.3;
-                } else if (cadastralValue > 300000000) {
+                if (cadastralValue <= condition_10000000) {
+                    return finalBidNoMagic_0_1;
+                } else if ((cadastralValue <= condition_20000000) & (condition_10000000 < cadastralValue)) {
+                    return finalBidNoMagic_0_15;
+                } else if ((cadastralValue <= condition_50000000) & (condition_20000000 < cadastralValue)) {
+                    return finalBidNoMagic_0_2;
+                } else if ((cadastralValue <= condition_300000000) & (condition_50000000 < cadastralValue)) {
+                    return finalBidNoMagic_0_3;
+                } else if (cadastralValue > condition_300000000) {
                 }
-                return 2;
+                return finalBidNoMagic_2;
             }
         },
         MoscowCar { //12
@@ -166,10 +189,10 @@ public class EnumSwitch {
             @Override
             public double changeFinalBid() {
                 System.out.println("MoscowCar");
-                if (cadastralValue > 300000000) {
-                    return 2;
+                if (cadastralValue > condition_300000000) {
+                    return finalBidNoMagic_2;
                 } else
-                    return 0.1;
+                    return finalBidNoMagic_0_1;
             }
         },
         Gorn { //14
@@ -177,10 +200,10 @@ public class EnumSwitch {
             @Override
             public double changeFinalBid() {
                 System.out.println("Gorn");
-                if (cadastralValue > 300000000) {
-                    return 0.7;
+                if (cadastralValue > condition_300000000) {
+                    return finalBidNoMagic_0_7;
                 } else
-                    return 0.3;
+                    return finalBidNoMagic_0_3;
             }
         }
     }
