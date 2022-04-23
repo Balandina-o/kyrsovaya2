0d964eaaba11fb41bc68fdba55305fb2e3fad8f2
AirstaNs <94823184+AirstaNs@users.noreply.github.com>
2022-04-23 18:42:51 +0500

    Убраны лишние элементы. Установка пути RegistrServlet

--- src/main/java/servlets/RegistrServlet.java
+++ src/main/java/servlets/RegistrServlet.java
@@ -12,53 +12,51 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * The Class RegistrServlet.
  */
+
 /**
  * @author balandina-o
- *
  */
 @WebServlet("/RegistrServlet")
-public class RegistrServlet extends HttpServlet{
+public class RegistrServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("UTF-8");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
 
-		String login, password, page="/Reg.jsp";
-		response.setContentType("text/html");
+        String login, password, page = "/Reg.jsp";
+        response.setContentType("text/html");
 
-		login = request.getParameter("login");
-		password = request.getParameter("password");//.trim();
+        login = request.getParameter("login");
+        password = request.getParameter("password");//.trim();
 
-		//TODO надо изменить - UtilServlets - checkLine
-		
 
-		if ((UtilServlets.checkLine(login) == true) & (UtilServlets.checkLine(password) == true)) {
-			String messageAuthZ = ManagerClient.apiReg(login, password);
+        UtilServlets.setPathToResources(request); //тест
 
-			//TODO установка пути - UtilServlets - setPath
-			if ("Зарегистрирован".equals(messageAuthZ)) {
-				request.getSession().setAttribute("role", "USER");
-				
-				response.sendRedirect(request.getContextPath() + "/Calc.jsp");
-				return;
-				
-			}else{
-				request.setAttribute("messageReg", messageAuthZ);
-			}
-		}else {
-			request.setAttribute("messageReg", "Логин и пароль - обязательные поля, которые не могут содержать пробелы и символ ;");
-		}
-		
-		if (request.getParameter("exitButton") != null) { // если нажата кнопка выхода 
-			response.sendRedirect(request.getContextPath() + "/Aut.jsp");
-			return;
-		}
-		
-		getServletContext().getRequestDispatcher(page).forward(request, response);//код перенаправления
-		return;
-	}
+        if ((UtilServlets.checkLine(login)) & (UtilServlets.checkLine(password))) {
+            String messageAuthZ = ManagerClient.apiReg(login, password);
 
+            if ("Зарегистрирован".equals(messageAuthZ)) {
+                request.getSession().setAttribute("role", "USER");
+
+                response.sendRedirect(request.getContextPath() + "/Calc.jsp");
+                return;
+
+            } else {
+                request.setAttribute("messageReg", messageAuthZ);
+            }
+        } else {
+            request.setAttribute("messageReg", "Логин и пароль - обязательные поля, которые не могут содержать пробелы и символ ;");
+        }
+
+        if (request.getParameter("exitButton") != null) { // если нажата кнопка выхода
+            response.sendRedirect(request.getContextPath() + "/Aut.jsp");
+            return;
+        }
+
+        getServletContext().getRequestDispatcher(page).forward(request, response);//код перенаправления
+        return;
+    }
 
 
 }
